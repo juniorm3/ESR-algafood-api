efd9dda98d01036822e65c69863006c357c7a989
Mario <mario.martins@sonda.com>
2020-03-27 12:34:39 -0300

    Buscando um objeto pelo id no banco de dados

--- src/main/java/com/algaworks/algafood/jpa/CadastroCozinha.java
+++ src/main/java/com/algaworks/algafood/jpa/CadastroCozinha.java
@@ -6,6 +6,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.algaworks.algafood.domain.model.Cozinha;
 
@@ -18,5 +19,14 @@ public class CadastroCozinha {
 	public List<Cozinha> listar(){
 		return manager.createQuery("from Cozinha", Cozinha.class)
 				.getResultList();
+	}
+	
+	public Cozinha buscar(Long id) {
+		return manager.find(Cozinha.class, id);
+	}
+	
+	@Transactional
+	public Cozinha adicionar(Cozinha cozinha) {
+		return manager.merge(cozinha);
 	}
 }
