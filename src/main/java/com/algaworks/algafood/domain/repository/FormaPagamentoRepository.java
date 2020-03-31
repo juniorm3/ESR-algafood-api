015c521e6eff0c29f45457684a04ee82afcc1b74
Mario <mario.martins@sonda.com>
2020-03-31 16:57:20 -0300

    Desafio: mapeando entidades

---
+++ src/main/java/com/algaworks/algafood/domain/repository/FormaPagamentoRepository.java
@@ -0,0 +1,16 @@
+package com.algaworks.algafood.domain.repository;
+
+import java.util.List;
+
+import com.algaworks.algafood.domain.model.FormaPagamento;
+
+public interface FormaPagamentoRepository {
+
+	List<FormaPagamento> todos();
+
+	FormaPagamento porId(Long id);
+
+	FormaPagamento addicionar(FormaPagamento formaPagamento);
+
+	void remover(FormaPagamento formaPagamento);
+}
